de58ec0bbc5da86a2eb6143d58138bf95bb5dee6
Otto Giron <ottog2486@gmail.com>
2018-03-07 23:53:44 -0600

    new change

--- app/brigade.js
+++ app/brigade.js
@@ -1,28 +1,9 @@
 const { events, Job } = require("brigadier");
 
 events.on("push   ", function(e, project) {
-  console.log("received push for commit " + e.commit)
+  console.log("\n\t==> RUNNING EVENT\n");
 
-  // Create a new job
-  var node = new Job("test-runner")
+  j1 = new Job("hello", "alpine:3.5", ["echo hello"])
 
-  // We want our job to run the stock Docker Python 3 image
-  node.image = "python:3"
-  node.useSource = true
-
-  // Now we want it to run these commands in order:
-  node.tasks = [
-    "cd /src/app",
-    "pip install -r requirements.txt",
-    "cd /src/",
-    "python setup.py test",
-    "ls"
-  ]
-
-  // We're done configuring, so we run the job
-  node.run().then(
-    (result) => {
-      console.log("\n job says", result.toString())
-    }
-  )
+  j1.run();
 })
