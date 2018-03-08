9d9eca6db7af29bd0956c459e72e9978418f9ee1
Otto Giron <ottog2486@gmail.com>
2018-03-08 00:11:35 -0600

    change brigade.js

--- app/brigade.js
+++ app/brigade.js
@@ -1,9 +1,9 @@
 const { events, Job } = require("brigadier");
 
-events.on("push   ", function(e, project) {
+events.on("exec   ", function(e, project) {
   console.log("\n\t==> RUNNING EVENT\n");
 
-  j1 = new Job("hello", "alpine:3.5", ["echo hello"])
+  j1 = new Job("hello", "alpine:3.5", ["echo 'hello'"])
 
   j1.run();
 })
